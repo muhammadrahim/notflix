307738d73e207f016709174a1201504642b7db42
Mo <rahim.muhammad@pm.me>
2021-12-09 06:37:18 +0100

    Remove exact keyword and refactor

---
+++ src/components/ListOfShows.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from '../Card';
+
+const ListOfShows = function () {
+  return (
+    <div className="ListOfShows">
+      <p>
+        Continue watching
+      </p>
+      <Card />
+    </div>
+  );
+};
+
+export default ListOfShows;
