60a1d1c0072bea98c173c8e5182ff9da776fc467
Mo <rahim.muhammad@pm.me>
2021-12-11 14:52:58 +0100

    Add nav bar with mui

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import AppBar from '@mui/material/AppBar';
-import { Link } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
+import IconButton from '@mui/material/IconButton';
 
 const Header = function () {
   return (
     <header>
-      <Link to="/"><h2>NOTFLIX</h2></Link>
-      <AppBar />
-      <ol><li>hi</li></ol>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            />
+            <Link color="inherit" underline="hover" href="/">NOTFLIX</Link>
+            <Button color="inherit" href="my-list">My List</Button>
+            <Button color="inherit" href="new">New And Popular</Button>
+            <Button color="inherit" href="auth">Sign up</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
     </header>
   );
 };
